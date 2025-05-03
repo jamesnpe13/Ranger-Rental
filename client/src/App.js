006edffb0d83faeb087458deedc864443014874c
James Elazegui <jameselazegui21@gmail.com>
2025-05-04 00:54:08 +1200

    Abstracts and Globals

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,19 @@
+import Button from "./components/Button";
+
 function App() {
   return (
     <>
-      <p>hello</p>
+      <h1>h1 Lorem Ipsum</h1>
+      <h2>h2 Lorem Ipsum</h2>
+      <h3>h3 Lorem Ipsum</h3>
+      <h4>h4 Lorem Ipsum</h4>
+      <p>p Lorem Ipsum</p>
+      <i>i Lorem Ipsum</i>
+      <Button type="primary" />
+      <Button type="secondary" />
+      <Button type="destructive" />
+      <Button type="greyed-out" />
+      <Button type="text" />
     </>
   );
 }
---
+++ client/src/components/Button.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Button.scss";
+
+export default function Button({ type }) {
+  return <div className={`Button ${type}`}>Button</div>;
+}
