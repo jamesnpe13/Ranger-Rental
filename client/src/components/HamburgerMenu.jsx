bced5d2d76013a192d2e75fd7aca41c75bc21748
James Elazegui <jameselazegui21@gmail.com>
2025-05-14 01:02:47 +1200

    Menu icons

--- client/src/components/HamburgerMenu.jsx
+++ client/src/components/HamburgerMenu.jsx
@@ -10,8 +10,6 @@ import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
 import { Link } from "react-router-dom";
 import routes from "../routes/Routes";
 
@@ -38,6 +36,7 @@ export default function HamburgerMenu() {
       <List>
         {routes.map((route) => (
           <ListItemButton key={route.path} component={Link} to={route.path}>
+            <ListItemIcon>{route.icon}</ListItemIcon>
             <ListItemText primary={route.label} />
           </ListItemButton>
         ))}
--- client/src/routes/Routes.js
+++ client/src/routes/Routes.js
@@ -1,13 +1,30 @@
 // Routes.js
 import SignIn from "../pages/SignIn";
 import Register from "../pages/Register";
-import NotFound from "../pages/NotFound";
 import Home from "../pages/Home";
+import HomeIcon from "@mui/icons-material/Home";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import CreateIcon from "@mui/icons-material/Create";
 
 const routes = [
-  { path: "/home", label: "Home", element: <Home /> },
-  { path: "/signin", label: "Sign In", element: <SignIn /> },
-  { path: "/register", label: "Register", element: <Register /> },
+  {
+    path: "/home",
+    label: "Home",
+    element: <Home />,
+    icon: <HomeIcon />,
+  },
+  {
+    path: "/signin",
+    label: "Sign In",
+    element: <SignIn />,
+    icon: <AccountBoxIcon />,
+  },
+  {
+    path: "/register",
+    label: "Register",
+    element: <Register />,
+    icon: <CreateIcon />,
+  },
 ];
 
 export default routes;
