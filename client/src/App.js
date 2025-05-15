d5696e11119745377e7e3d81a6de11dc2e336cda
James Elazegui <jameselazegui21@gmail.com>
2025-05-16 01:01:45 +1200

    NavLink active link

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 import HeaderBar from "./components/HeaderBar";
 import FooterBar from "./components/FooterBar";
+import { React } from "react";
 
 export default function App() {
   return (
