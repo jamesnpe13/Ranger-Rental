ac3b3550ba85e731b7633fc515a364157139bb23
James Elazegui <jameselazegui21@gmail.com>
2025-05-16 12:59:13 +1200

    react integration maps

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,31 @@
 import React from "react";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+
+const containerStyle = {
+  width: "100%",
+  height: "500px",
+};
+
+const center = {
+  lat: -36.8485,
+  lng: 174.7633,
+};
 
 export default function Home() {
   return (
     <div className="page">
       <section>
         <p>Home</p>
+
+        <LoadScript googleMapsApiKey="AIzaSyB41EEwAtfEay86VMXN5r5UJ6qILh0Lk44">
+          <GoogleMap
+            mapContainerStyle={containerStyle}
+            center={center}
+            zoom={13}
+          >
+            <Marker position={center} />
+          </GoogleMap>
+        </LoadScript>
       </section>
     </div>
   );
