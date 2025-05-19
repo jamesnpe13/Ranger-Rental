be82b08f1ceab315a2a09174200ebba85e6f5692
James Elazegui <jameselazegui21@gmail.com>
2025-05-19 16:05:50 +1200

    Location labels

--- client/src/components/HeaderBar.jsx
+++ client/src/components/HeaderBar.jsx
@@ -3,17 +3,21 @@ import "./HeaderBar.scss";
 import HamburgerMenu from "./HamburgerMenu";
 import logo from "../assets/images/logo-white-simple.png";
 import { useNavigate, useLocation } from "react-router-dom";
+import routes from "../routes/Routes";
 
 export default function HeaderBar() {
   const location = useLocation();
   const navigate = useNavigate();
+
+  const match = routes.find((route) => route.path === location.pathname);
+  const label = match ? match.label : "Unknown Page";
 
   return (
     <div className="header-bar">
       <div className="logo-container" onClick={() => navigate("/")}>
         <img className="logo" src={logo} alt="" />
       </div>
-      <p className="location">Temp loc: "{location.pathname}"</p>
+      <p className="location">{label}</p>
       <HamburgerMenu />
     </div>
   );
