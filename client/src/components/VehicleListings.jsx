08fcdafc857f2a537d8d09cfbda772d24b12fcd5
James Elazegui <jameselazegui21@gmail.com>
2025-05-20 16:06:46 +1200

    vehicles slice/reducer

--- client/src/components/VehicleListings.jsx
+++ client/src/components/VehicleListings.jsx
@@ -1,38 +1,22 @@
 import React from "react";
 import "./VehicleListings.scss";
+import { useSelector, useDispatch } from "react-redux";
 
 export default function VehicleListings() {
-  const vehicleList = [
-    {
-      make: "Toyota",
-      model: "Vitz",
-      year: "2018",
-      milleage: 123000,
-    },
-    {
-      make: "Honda",
-      model: "Accord",
-      year: "2014",
-      milleage: 200189,
-    },
-    {
-      make: "Mazda",
-      model: "Verisa",
-      year: "2004",
-      milleage: 106999,
-    },
-  ];
+  const vehicles = useSelector((state) => state.vehicles);
+  const dispatch = useDispatch();
+
   const vehicleMap = () => {
     return (
       <div className="vehicle-list-container">
-        {vehicleList.map((item, index) => (
+        {vehicles.vehicles.map((item, index) => (
           <VehicleListItem key={index} details={item} />
         ))}
       </div>
     );
   };
 
-  return vehicleList?.length !== 0 ? (
+  return vehicles.vehicles?.length !== 0 ? (
     vehicleMap()
   ) : (
     <p>There are no vehicles to display.</p>
