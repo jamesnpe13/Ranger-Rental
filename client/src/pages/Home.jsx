cd17950a362771fbb77e98430f1de41cf931545a
James Elazegui <jameselazegui21@gmail.com>
2025-05-30 09:39:22 +1200

    update

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,11 +7,24 @@ import { useSelector, useDispatch } from "react-redux";
 import GuestHome from "./GuestHome";
 import CustomerHome from "./CustomerHome";
 import AdminHome from "./AdminHome";
+import axios from "axios";
+import { useEffect } from "react";
 
 export default function Home() {
   const navigate = useNavigate();
   const auth = useSelector((state) => state.auth);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    axios
+      .get("/api/items")
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
 
   switch (auth.user.role) {
     case "customer":
